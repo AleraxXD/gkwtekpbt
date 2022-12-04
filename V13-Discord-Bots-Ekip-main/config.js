d9aea7ffa23325ff3eea2357c80952b918d5cda3
AleraxXD <99982851+AleraxXD@users.noreply.github.com>
2022-12-04 23:20:54 +0300

    ekipp
    ..

---
+++ V13-Discord-Bots-Ekip-main/config.js
@@ -0,0 +1,17 @@
+﻿const config = {
+  "botStatus": ["Created by Gökw."],
+  "prefix": ["!", "."],
+  "token": "",
+  "mongoose": "",
+  "botOwners": ["937434889614151720"],
+  "guildID": "",
+  
+  "emojis": {
+    "yes_name": "yes_gokw",
+    "no_name": "no_gokw",
+    "mute_name": "gokw_mute"
+  }
+  }
+  
+  module.exports = config;
+  
